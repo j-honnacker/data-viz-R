3ed5d3871ba6be653e63a956d16965fe640aff59
Joerg Honnacker <joerghonnacker@gmail.com>
2020-02-12 16:10:41 +0100

    Create 'dataviz_stg1_rhine_wl.R'

---
+++ dataviz_stg1_rhine_wl.R
@@ -0,0 +1,15 @@
+library(readr)
+library(ggplot2)
+
+# set working directory
+'setwd("/Users/jhonnacker/data-viz-R")'
+
+
+# read data
+rhine_wl_df <- read_csv( file = "data_stg1/rhine_wl_1996-2018.csv" )
+
+
+# LINE CHART ----
+rhine_wl_df %>%
+  ggplot( aes(x=date, y=water_level) ) +
+    geom_line(na.rm=TRUE)
